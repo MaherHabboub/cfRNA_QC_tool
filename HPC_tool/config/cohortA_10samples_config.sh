fad0b0d3230a0b0cec04b785e68a943876532006
MaherHabboub <maher.habboub@gmail.com>
2026-06-10 17:53:01 +0200

    updated

--- HPC_tool/config/cohortA_10samples_config.sh
+++ HPC_tool/config/cohortA_10samples_config.sh
@@ -4,14 +4,15 @@
 # Cohort A 10-sample QC config
 # ============================================================
 
+# Input files/directories
 SAMPLESHEET="/scratch/gent/vo/000/gvo00027/projects/MHB/Cohort_A/QC_master/inputs/cohortA_10samples_samplesheet.tsv"
-
 FASTQ_DIR="/scratch/gent/vo/000/gvo00027/projects/MHB/Cohort_A/fastq"
 BAM_DIR="/scratch/gent/vo/000/gvo00027/projects/MHB/Cohort_A/map"
 
-
+# Reference files
 GTF="/data/gent/vo/000/gvo00027/resources/Ensembl_transcriptomes/Homo_sapiens/GRCh38/Homo_sapiens.GRCh38.109.chrIS_spikes_45S.gtf"
-OUTDIR="/scratch/gent/vo/000/gvo00027/projects/MHB/Cohort_A/QC_master"
-
-BED12="/scratch/gent/vo/000/gvo00027/projects/MHB/Cohort_A/QC_master/annotation/Homo_sapiens.GRCh38.109.chrIS_spikes_45S.bed12.bed"
 EXON_BED="/data/gent/vo/000/gvo00027/resources/Ensembl_bedregions/Homo_sapiens/GRCh38/Homo_sapiens.GRCh38.109.chrIS_spikes_45S_exons_sorted_merged.bed"
+
+# Output directory
+OUTDIR="/scratch/gent/vo/000/gvo00027/projects/MHB/Cohort_A/QC_master"
+BED12="${OUTDIR}/annotation/Homo_sapiens.GRCh38.109.chrIS_spikes_45S.bed12.bed"
